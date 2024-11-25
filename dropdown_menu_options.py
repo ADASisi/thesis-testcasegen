4ead75d2d7e58f8b8994f84244ad380b40e7d554
santova <silviaadantova@gmail.com>
2024-11-25 22:45:47 +0200

    adding dropdown file that will get rule names and show their description

---
+++ dropdown_menu_options.py
@@ -0,0 +1,18 @@
+def get_rules_name():
+    options_dropdown_menu = []
+    with open("65LPe_V1830.psv", "r") as fpsv:
+        for line in fpsv:
+            split_text = line.strip().split("|")
+            if len(split_text) > 2:
+                options_dropdown_menu.append(split_text[2])
+    return options_dropdown_menu
+
+
+def show_rule_description(rule_name):
+    description_rule = ""
+    with open("65LPe_V1830.psv", "r") as fpsv:
+        for line in fpsv:
+            split_text = line.split("|")
+            if split_text[2].strip() == rule_name:
+                description_rule = split_text[4].strip() + split_text[5].strip() + split_text[6].strip()
+                return description_rule
