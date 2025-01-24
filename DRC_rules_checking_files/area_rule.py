3c897948c08cc9e805c1068550b9049a32f90cc0
santova <silviaadantova@gmail.com>
2025-01-24 20:36:08 +0200

    add max area checking in file with min area

---
+++ DRC_rules_checking_files/area_rule.py
@@ -0,0 +1,73 @@
+import gdspy
+import basic_functions
+
+lib = gdspy.GdsLibrary()
+
+results_min = []
+results_max = []
+
+
+def display_test_cases(i, actual_area, rule_area, rule_type):
+    if rule_type == "min":
+        if round(actual_area, 3) < rule_area:
+            return f"Test case {i + 1}: Area = {round(actual_area, 3)} (Fail)"
+        else:
+            return f"Test case {i + 1}: Area = {round(actual_area, 3)} (Pass)"
+    elif rule_type == "max":
+        if round(actual_area, 3) > rule_area:
+            return f"Test case {i + 1}: Area = {round(actual_area, 3)} (Fail"
+        else:
+            return f"Test case {i + 1}: Area = {round(actual_area, 3)} (Pass)"
+
+
+def export_file_area_rule(lib, layer_name, rule_type):
+    gds_filename = f"{layer_name}_{rule_type}_area_test.gds"
+    lib.write_gds(gds_filename)
+
+
+def display_file_area(cell):
+    gdspy.LayoutViewer(cells=cell)
+
+
+def check_area_rule(rule_area, layer_name, num_layer, datatype, rule_type):
+    global min_rule_cell, max_rule_cell
+    cell_name = f"{layer_name}_{rule_type.upper()}_AREA_TEST"
+    results = results_min if rule_type == "min" else results_max
+
+    if basic_functions.cell_exists(lib, cell_name) is False:
+        if rule_type == "min":
+            min_rule_cell = lib.new_cell(cell_name)
+        if rule_type == "max":
+            max_rule_cell = lib.new_cell(cell_name)
+
+        areas = basic_functions.generate_value(rule_area)
+
+        for i, area in enumerate(areas):
+            layer_specifications = basic_functions.get_layer_other_parameters(layer_name)
+            width = layer_specifications["width"]
+            height = area / width
+            points = basic_functions.generate_polygon_points(
+                basic_functions.base + i * width * 2, basic_functions.base, width, height
+            )
+            polygon = gdspy.Polygon(points, layer=num_layer, datatype=datatype)
+            if rule_type == "min":
+                min_rule_cell.add(polygon)
+            if rule_type == "max":
+                max_rule_cell.add(polygon)
+
+            actual_area = width * height
+
+            results.append(display_test_cases(i, actual_area, rule_area, rule_type))
+
+    if rule_type == "min":
+        return results, lib, min_rule_cell
+    if rule_type == "max":
+        return results, lib, max_rule_cell
+
+
+def check_minimum_area(min_area, layer_name, num_layer, datatype):
+    return check_area_rule(min_area, layer_name, num_layer, datatype, rule_type="min")
+
+
+def check_maximum_area(max_area, layer_name, num_layer, datatype):
+    return check_area_rule(max_area, layer_name, num_layer, datatype, rule_type="max")
